fd2ffadacd1d488937cdb676c5077adad9b4fc97
Matt Tomlinson <mtomlinson@navexglobal.com>
2018-03-13 09:57:56 -0600

    2.2

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -11,11 +11,11 @@ namespace GradeBook.GradeBooks
 {
     public class BaseGradeBook
     {
-		public GradeBookType Type;
         public string Name { get; set; }
         public List<Student> Students { get; set; }
+		public GradeBookType Type;
 
-        public BaseGradeBook(string name)
+		public BaseGradeBook(string name)
         {
             Name = name;
             Students = new List<Student>();
